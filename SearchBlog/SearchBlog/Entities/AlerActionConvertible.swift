5d8438001921f7241c7da59d01d9b0d63bfa69bf
alsldmswl <jej1452@gmail.com>
2023-04-01 18:14:56 +0900

    Add new project: SearchBlog - 기본 UI 구성

---
+++ SearchBlog/SearchBlog/Entities/AlerActionConvertible.swift
@@ -0,0 +1,13 @@
+//
+//  AlerActionConvertible.swift
+//  SearchBlog
+//
+//  Created by eun-ji on 2023/04/01.
+//
+
+import UIKit
+
+protocol AlerActionConvertible {
+    var title: String {get}
+    var style: UIAlertAction.Style {get}
+}
---
+++ SearchBlog/SearchBlog/Entities/BolgListCellData.swift
@@ -0,0 +1,16 @@
+//
+//  BolgListCellData.swift
+//  SearchBlog
+//
+//  Created by eun-ji on 2023/04/01.
+//
+
+import Foundation
+
+
+struct BlogListCellData {
+    let thumbnailURL: URL?
+    let name: String?
+    let title: String?
+    let datetime: Date?
+}
